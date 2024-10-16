a56c545f4b5fda9b6ba07aae0020cdf8d9285e8f
Sele <seleluciano4@gmail.com>
2024-10-15 21:05:28 -0300

    MVC

--- MVC/routes/userRoutes.js
+++ MVC/routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/userController');
 
 // Rutas para manejar usuarios
 router.get('/', userController.index); // Ruta para listar usuarios
-router.post('/create', userController.create); // Ruta para crear un usuario
-router.post('/delete/:email', userController.delete); // Ruta para eliminar un usuario por email
+router.post('/users/create', userController.create); // Ruta para crear un usuario
+router.post('/users/delete/:email', userController.delete); // Ruta para eliminar un usuario por email
 
 module.exports = router;
